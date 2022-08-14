f2f3cbbec2d5e4a4b94d89ec30c2eb8b4a3f334b
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 14:51:09 +0300

    gitautosync update 34

--- mythread1.py
+++ mythread1.py
@@ -3,13 +3,13 @@
 import threading
 
 
-def hello():
-    print('Hello!')
+def hello(n):
+    print(f'{n} Hello!')
 
 
 for i in range(10):
     # create a thread object, which will run hello
-    t = threading.Thread(target=hello)
+    t = threading.Thread(target=hello, args=(i,))  # args is a tuple
 
     # actually run the function in a new thread
     t.start()
