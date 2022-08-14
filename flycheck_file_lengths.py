5b58832018f131c5d2e38e18e543268bbd2e85be
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 15:32:34 +0300

    gitautosync update 96

---
+++ flycheck_file_lengths.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import glob
+
+
+def file_length(filename):
+    total = 0
+
+    with open(filename) as f:
+        for one_line in f:
+            total += len(one_line)
+
+    return total
+
+
+file_length_total = 0
+
+with ThreadPoolExecutor() as executor:
+    all_futures = []
+    for one_filename in glob.glob('*.txt'):
+        future = executor.submit(file_length, one_filename)
+        all_futures.append(future)
+
+    for one_future in as_completed(all_futures):
+        file_length_total += one_future.result()    # result is a future method
+
+print(f'Total is {file_length_total}')
