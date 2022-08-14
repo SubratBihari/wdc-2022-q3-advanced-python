11ca77c477b035b3f80d3c547264e17acf1046d3
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 14:58:32 +0300

    gitautosync update 44

---
+++ flycheck_mythread2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# simple producer-consumer, with a queue
+
+import threading
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    print(f'{n} Hello!')
+    print(f'{n} Goodbye!')
+
+
+all_threads = []
+for i in range(10):
+    # create a thread object, which will run hello
+    t = threading.Thread(target=hello, args=(i,))  # args is a tuple
+
+    # keep track of this new thread
+    all_threads.append(t)
+
+    # actually run the function in a new thread
+    t.start()
+
+# if we call "join" on a thread object, we wait for it to finish
+for one_thread in all_threads:
+    one_thread.join()
+
+
+print('Done!')
