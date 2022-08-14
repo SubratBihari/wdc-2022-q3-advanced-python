c6aa8bd4f7767d57118f5a8b9772f8b95b379a09
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 15:08:53 +0300

    gitautosync update 61

---
+++ flycheck_mythread4.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# future -- a proxy for an answer that will (eventually) come
+
+import threading
+import time
+import random
+
+from concurrent.futures import ThreadPoolExecutor, submit
+
+
+def hello(n):
+    print(f'{n} Hello!')
+    print(f'{n} Goodbye!')
+
+with ThreadPoolExecutor() as executor:
+    for i in range(10):
+        result = submit(hello, args=(i,))
+
+        
+
+
+
+
+all_threads = []
+for i in range(10):
+    # create a thread object, which will run hello
+    t = threading.Thread(target=hello, args=(i,))  # args is a tuple
+
+    # keep track of this new thread
+    all_threads.append(t)
+
+    # actually run the function in a new thread
+    t.start()
+
+# if we call "join" on a thread object, we wait for it to finish
+for one_thread in all_threads:
+    one_thread.join()
+
+
+print('Done!')
