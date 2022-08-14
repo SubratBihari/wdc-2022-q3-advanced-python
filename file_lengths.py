a8a8c723ee55e0a88278e3d480313099fedbfc84
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 15:29:54 +0300

    gitautosync update 87

--- file_lengths.py
+++ file_lengths.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import glob
 
 
 def file_length(filename):
     total = 0
 
-    for one_line in open(filename):
-        total += len(one_line)
+    with open(filename) as f:
+        for one_line in f:
+            total += len(one_line)
 
     return total
 
 
 with ThreadPoolExecutor() as executor:
     all_results = []
-    for i in range(10):
-        result = executor.submit(hello, i)
+    for one_filename in glob.glob('*.txt'):
+        result = executor.submit(file_length, one_filename)
         all_results.append(result)
 
     for one_result in as_completed(all_results):
