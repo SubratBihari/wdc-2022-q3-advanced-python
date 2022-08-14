16dced894b95544f63d182739a3c5c115c7f0e84
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 15:28:10 +0300

    gitautosync update 81

--- flycheck_file_lengths.py
+++ flycheck_file_lengths.py
@@ -4,6 +4,11 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def file_length(filename):
+    total = 0
+
+    for one_line in open(filename):
+        
+
 
 
 with ThreadPoolExecutor() as executor:
