7ae4a1e1b9a3f06861ae8c219a621eb3c58084ae
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 15:37:22 +0300

    gitautosync update 104

---
+++ file_lengths_processes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import glob
+
+
+def file_length(filename):
+    total = 0
+
+    with open(filename) as f:
+        for one_line in f:
+            total += len(one_line)
+
+    return total
+
+
+file_length_total = 0
+
+with ThreadPoolExecutor() as executor:
+    results = executor.map(file_length, glob.glob('*.txt'))
+
+print(f'Total is {sum(results)}')
