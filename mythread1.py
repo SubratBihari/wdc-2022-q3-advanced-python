2bafe2199b2df306bd7c172c8919db664b279f8e
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 14:56:20 +0300

    gitautosync update 43

--- mythread1.py
+++ mythread1.py
@@ -11,11 +11,20 @@ def hello(n):
     print(f'{n} Goodbye!')
 
 
+all_threads = []
 for i in range(10):
     # create a thread object, which will run hello
     t = threading.Thread(target=hello, args=(i,))  # args is a tuple
 
+    # keep track of this new thread
+    all_threads.append(t)
+
     # actually run the function in a new thread
     t.start()
+
+# if we call "join" on a thread object, we wait for it to finish
+for one_thread in all_threads:
+    one_thread.join()
+
 
 print('Done!')
