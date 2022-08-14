ce292b5e0d8dc8f88d80511738bc98f79cf7c056
Reuven Lerner <reuven@lerner.co.il>
2022-08-14 15:12:25 +0300

    gitautosync update 71

--- mythread4.py
+++ mythread4.py
@@ -13,7 +13,7 @@ def hello(n):
 with ThreadPoolExecutor() as executor:
     all_results = []
     for i in range(10):
-        result = executor.submit(hello, args=(i,))
+        result = executor.submit(hello, i)
         all_results.append(result)
 
     for one_result in as_completed(all_results):
